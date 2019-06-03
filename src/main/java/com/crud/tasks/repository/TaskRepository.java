182d88d4caa08b35ba6d90323159ef85c6aca2c2
mm.kucharski@gmail.com <mm.kucharski@gmail.com>
2019-06-03 14:53:40 +0200

    23.4 @schedule

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -19,4 +19,7 @@ public interface TaskRepository extends CrudRepository<Task, Long> {
     Task save(Task task);
 
     void deleteTaskById(Long id);
+
+    @Override
+    long count();
 }
---
+++ src/main/java/com/crud/tasks/shceduler/EmailScheduler.java
@@ -0,0 +1,33 @@
+package com.crud.tasks.shceduler;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailScheduler {
+    @Autowired
+    private SimpleEmailService simpleEmailService;
+    @Autowired
+    private TaskRepository repository;
+    @Autowired
+    private AdminConfig config;
+
+    private static final String SUBJECT = "Tasks: Once a day mail";
+
+//    @Scheduled(cron = "0 0 10 * * *")
+    @Scheduled(fixedDelay = 10000)
+    public void sendInformationEmail() {
+        long size = repository.count();
+        simpleEmailService.send(new Mail(
+                config.getAdminMail(),
+                SUBJECT,
+                "Currently in database you got: " + size + " tasks"
+        ));
+    }
+
+}
